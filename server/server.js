ede39780388aa224eaf9bae74d152b615ffe96d1
Matt Latham <latham75@gmail.com>
2016-09-26 09:33:01 -0500

    init with express, client index

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 7575
 app.set('port', port)
 
 //middleware
-app.use(express.static(//))
+app.use(express.static('client'))
 
 
-app.listen(port, () => console.log("Listening on port: ${port}"))
+app.listen(port, () => console.log(`Listening on port: ${port}`))
